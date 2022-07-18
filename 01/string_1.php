abb09b541876edccac8c8a7ec2e056eabca70968
“chie-hirabayashi” <nyantoroman@yahoo.co.jp>
2022-07-18 14:42:21 +0900

    PHPの基礎1の課題完了

---
+++ 01/string_1.php
@@ -0,0 +1,4 @@
+<?php 
+
+echo nl2br('私の好きな食べ物は' .PHP_EOL);
+echo 'たけのこです';
---
+++ 01/string_2.php
@@ -0,0 +1,10 @@
+<?php 
+
+$food = 'りんご';
+$drink = 'みかんジュース';
+
+$break_fast = $food .'と' .$drink;
+
+echo $break_fast;
+
+?>
